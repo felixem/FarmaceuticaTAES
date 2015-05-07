5c47b47a5ecd3733b15ef465a4785e6e9bcd3050
John <jacv050@gmail.com>
2015-05-07 17:18:47 +0200

    Toolbar como ActionBarn enlazada

--- android-project/FarmaceuticaTAES/app/src/main/java/farmaceutica/taes/farmaceutica/presentacion/controlador/Impl/MainActivity.java
+++ android-project/FarmaceuticaTAES/app/src/main/java/farmaceutica/taes/farmaceutica/presentacion/controlador/Impl/MainActivity.java
@@ -1,20 +1,25 @@
 package farmaceutica.taes.farmaceutica.presentacion.controlador.Impl;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarActivity;
 
 import farmaceutica.taes.farmaceutica.R;
 
 /**
  * Created by john on 7/05/15.
  */
-public class MainActivity extends Activity{
+public class MainActivity extends ActionBarActivity {
 
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.main_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
+
+        
+        // Set an OnMenuItemClickListener to handle menu item clicks
     }
 
 }
