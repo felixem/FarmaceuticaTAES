b0cbfb4aec9a1bda187535e95e7937c650f93954
Javier Ortega <fantasma345@hotmail.es>
2015-05-12 19:21:15 +0200

    MedicoRepository obtener medico por id

--- android-project/FarmaceuticaTAES/app/src/main/java/farmaceutica/taes/farmaceutica/presentacion/controlador/Impl/util/ListaVisitas.java
+++ android-project/FarmaceuticaTAES/app/src/main/java/farmaceutica/taes/farmaceutica/presentacion/controlador/Impl/util/ListaVisitas.java
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import farmaceutica.taes.domainmodel.Model.Visita;
+import farmaceutica.taes.domainmodel.Repository.MedicoRepository;
 import farmaceutica.taes.farmaceutica.R;
 import farmaceutica.taes.farmaceutica.presentacion.controlador.Impl.util.app.fachadas.FachadaComunicador;
 import farmaceutica.taes.farmaceutica.presentacion.controlador.Impl.util.app.fachadas.FachadaVisita;
@@ -21,6 +24,7 @@ public class ListaVisitas extends ActivityBase {
     private FachadaVisita fachadaVisita;
     private FachadaComunicador fachadaComunicador;
     private Class<?> destino;
+    private MedicoRepository repositorio;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,9 +35,16 @@ public class ListaVisitas extends ActivityBase {
         fachadaVisita = new FachadaVisita();
         fachadaComunicador = new FachadaComunicador();
 
-        txt= (TextView) findViewById(R.id.txt_title2);
+        txt= (TextView) findViewById(R.id.txt_visita_elegida);
         visita = fachadaComunicador.RecibirUniversidadPosicion0();
-        txt.setText("Visita: " + visita.getFechaVisita().toString() +" con el doctor: " + visita.getMedico().getNombre() );
+        txt.setText("Visita: " + visita.getFechaVisita().toString() +" con el doctor: " + repositorio.getMedicoById(visita.getMedico().getId()).getNombre() + " " + repositorio.getMedicoById(visita.getMedico().getId()).getApellidos());
+
+        destino = null;
+
+        //Creamos el adaptador para el ListView
+        ArrayList<Visita> visitas = fachadaVisita.obtenerVisitas(this, visita);
+
+
 
     }
 
