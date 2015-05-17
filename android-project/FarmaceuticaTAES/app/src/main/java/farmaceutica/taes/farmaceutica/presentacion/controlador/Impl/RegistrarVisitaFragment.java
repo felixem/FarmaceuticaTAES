250713260f83d64ab8cd8372dd6e31040d5d4283
John <jacv050@gmail.com>
2015-05-17 16:57:47 +0200

    Dialog style

--- android-project/FarmaceuticaTAES/app/src/main/java/farmaceutica/taes/farmaceutica/presentacion/controlador/Impl/RegistrarVisitaFragment.java
+++ android-project/FarmaceuticaTAES/app/src/main/java/farmaceutica/taes/farmaceutica/presentacion/controlador/Impl/RegistrarVisitaFragment.java
@@ -335,7 +335,7 @@ public class RegistrarVisitaFragment extends BaseFragment{
     private void crearMaterialEntregado(View view)
     {
         // custom dialog
-        final Dialog dialog = new Dialog(getActivity());
+        final Dialog dialog = new Dialog(getActivity(), R.style.AppTheme_Dialog);
         dialog.setContentView(R.layout.dialog_crear_visita_material);
         dialog.setTitle("Añadir material entregado");
 
